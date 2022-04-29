c52349c5d73181bacd9fe735d52c6a3e72afd1d2
Amir <amir.hackett@gmail.com>
2022-04-29 17:29:06 -0400

    added function to validate notes

--- routes/apiRoutes/notesRoute.js
+++ routes/apiRoutes/notesRoute.js
@@ -1,7 +1,6 @@
 const router = require('express').Router()
-const { v4: uuidv4 } = require('uuid')
 const { notes } = require('../../db/db')
-const { findById, createNote, editNote, deleteNote } = require('../../lib/notes')
+const { findById, validateNote, createNote, deleteNote} = require('../../lib/notes')
 
 router.get('/notes', (req, res) => {
     res.json(notes)
@@ -10,15 +9,20 @@ router.get('/notes', (req, res) => {
 //route listens for a post request
 router.post('/notes', (req, res) => {
     
-    if (!req.body.id) {
-        req.body.id = uuidv4()
-        createNote(req.body, notes)
+    // set id based on what the next index of the array will be 
+    req.body.id = notes.length.toString(); 
+ 
+    // if any data in req.body is incorrect, send error
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.'); 
+    
     } else {
-        editNote(req.body, notes)
+        // add note to json file and animals array in this function 
+        const note = createNote(req.body, notes); 
+ 
+        res.json(note);
     }
-
-    res.json(req.body)
-})
+ })
 
 //route listens for the delete request
 router.delete('/notes/:id', (req, res) => {
